682591190260322be46ae2decdad5dc1acd0da78
buyoh <buyoh@users.noreply.github.com>
2025-10-12 12:07:41 +0900

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+# pandoc filters
+
+## features
+
+Convert following cases
+
+- markdown -> redmine style textile
+
+## How to use
+
+```
+pandoc input.md -o out.textile --filter=dist/ToRedmine.js 
+```
---
+++ src/ToRedmine.ts
@@ -0,0 +1,25 @@
+import { RawBlock, RawInline, AnyElt, PandocMetaMap, toJSONFilter } from 'pandoc-filter';
+
+//
+
+function action(
+  elem: AnyElt,
+  format: string,
+  meta: PandocMetaMap
+): Promise<AnyElt | Array<AnyElt> | void> | AnyElt | Array<AnyElt> | void {
+  if (elem.t === 'CodeBlock') {
+    const [[, lang], code] = elem.c;
+    const syntax = lang.length ? ` class="${lang}"` : '';
+    // const escapedCode = code.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    if (`${lang}` === 'plantuml') {
+      return RawBlock('html', `{{plantuml\n${code}\n}}\n`);
+    } else {
+      return RawBlock('html', `<pre><code${syntax}>\n${code}</code></pre>\n`);
+    }
+  } else if (elem.t === 'Code') {
+    const [, code] = elem.c;
+    return RawInline('html', `<code>${code}</code>`);
+  }
+}
+
+toJSONFilter(action);
